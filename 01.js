233504dd7a2fe95dd34fb6b0da869548ed91862e
Nils Riedemann <nils@bleepbloop.studio>
2021-12-01 14:16:12 +0100

    add and run prettier

--- 01.js
+++ 01.js
@@ -1,13 +1,17 @@
-import { readFileSync } from 'fs';
-import { pipe, inToArr, map, sum } from './helpers.js';
+import { readFileSync } from "fs";
+import { pipe, inToArr, map, sum } from "./helpers.js";
 
 // -- Part A ---------------------------------------------------------------
-const countIncrements = (input) => input.reduce((prev, cv) => ({
-    increments: prev.value < cv ? prev.increments + 1 : prev.increments,
-    value: cv
-}), {increments: 0}).increments
+const countIncrements = input =>
+  input.reduce(
+    (prev, cv) => ({
+      increments: prev.value < cv ? prev.increments + 1 : prev.increments,
+      value: cv,
+    }),
+    { increments: 0 }
+  ).increments;
 
-const getIncrements = pipe(inToArr, countIncrements)
+const getIncrements = pipe(inToArr, countIncrements);
 
 // Verify via sample
 const sample = `199
@@ -19,27 +23,22 @@ const sample = `199
 240
 269
 260
-263`
+263`;
 
-console.assert(getIncrements(sample) === 7, "sample does not expectation")
+console.assert(getIncrements(sample) === 7, "sample does not expectation");
 
 // Calculate with actual input
-const input = readFileSync('./01.input', 'utf-8')
-console.log('Day 1, Part 1:', getIncrements(input))
+const input = readFileSync("./01.input", "utf-8");
+console.log("Day 1, Part 1:", getIncrements(input));
 
 // -- Part B----------------------------------------------------------------
-const toTriplets = (input) => 
-  input.reduce((acc, cv, i, arr) => {
+const toTriplets = input =>
+  input.reduce((acc, _, i, arr) => {
     if (i + 3 > arr.length) return acc;
-    return acc.concat([arr.slice(i, i + 3)])
-}, [])
+    return acc.concat([arr.slice(i, i + 3)]);
+  }, []);
 
-const windowedIncrements = pipe(
-  inToArr,
-  toTriplets,
-  map(sum),
-  countIncrements
-)
+const windowedIncrements = pipe(inToArr, toTriplets, map(sum), countIncrements);
 
-console.assert(windowedIncrements(sample) === 5, "sample does not expectation")
-console.log( "Day 1, Part 2:", windowedIncrements(input))
+console.assert(windowedIncrements(sample) === 5, "sample does not expectation");
+console.log("Day 1, Part 2:", windowedIncrements(input));
