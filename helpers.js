0f1f99f921ec9c99095db9ee45d0007904bad0b9
= <=>
2021-12-04 18:51:39 +0100

    Day 04
    This felt very similar to yesterday - with a little added complexity.
    Finally had a proper use case for `until`, which I had been curious
    about in real world applications.
    
    Lost a bunch of time because I didn't properly test my
    checkRow/checkCols functions that I just assumed to work. Writing tests
    earlier for those functions too.

--- helpers.js
+++ helpers.js
@@ -1,6 +1,5 @@
 import fs from "fs";
 
-// -- FP utils -------------------------------------------------------------
 export const pipe = (...fns) => (...initParams) => initParams.length > 1 
   ? fns.slice(1).reduce((p, f) => f(p), fns[0].apply(this, initParams))
   : fns.reduce((p, f) => f(p), initParams[0]);
@@ -11,33 +10,39 @@ export const curry = fn => function curried(...args) {
     }
 export const reduce = curry((fn, initialValue, array) => array.reduce(fn, initialValue))
 export const map = fn => functor => functor.map(fn)
+export const flatten = arr => arr.flat(Infinity)
 export const apply = fn => args => fn.apply(this, args)
 export const filter = curry((predicate, arr) => arr.filter(predicate))
+export const complement = f => (...args) => !f.apply(this, args)
+export const reject = curry((predicate, arr) => arr.filter(i => !predicate(i)))
 export const id = id => id
 export const get = p => o => o[p]
+export const length = get('length')
 export const pluck = p => map(get(p))
 export const converge = curry((convergeFn, branchFns, param) => 
   convergeFn.apply(this, branchFns.map(fn => fn(param))))
 export const list = (...args) => args
 export const transpose = arr => map(col => map(row => row[col])(arr))(Object.keys(arr[0]))
 export const K = value => () => value // aka always
-
-// utils
+export const every = curry((pred, arr) => arr.every(pred))
+export const some = curry((pred, arr) => arr.some(pred))
+export const T = true
+export const isTrue = b => b === true
 export const max = arr => Math.max.apply(Math, arr)
 export const maxBy = f => ([a, b]) => f(b) > f(a) ? b : a
 export const minBy = f => ([a, b]) => f(b) < f(a) ? b : a
-export const join = glue => arr => arr.join(glue)
 export const parseBinary = s => parseInt(s, 2)
-export const multiply = (a, b) => a * b
+export const toNumber = s => Number(s)
+export const multiply = curry((a, b) => a * b)
 export const tap = fn => (...args) => pipe(fn.apply(fn.context, args), K(args))
-export const tapLog = prefix => arg => {
-  console.log(prefix, toJson(arg))
-  return arg
+export const tapLog = prefix => (...args) => {
+  if (args.length > 1) console.warn('Note: tapLog has been called with more than 1 argument')
+  console.log(prefix, toJson(args));
+  return args[0]
 }
-export const inspect = arg => {
-  console.log("-- inspect: ", toJson(arg))
-  return arg
-}
+export const inspect = tapLog("\n\n-- inspect:\n")
+export const join = glue => arr => arr.join(glue)
+export const trim = s => s.trim()
 export const split = delim => string => string.split(delim)
 export const splitLines = split('\n')
 export const splitChars = split('')
@@ -55,7 +60,12 @@ export const countValues = arr => arr.reduce((acc, cv) => {
   acc[cv]++;
   return acc;
 }, {})
-
+export const until = curry((pred, fn, init) => {
+  let val = init;
+  while(!pred(val)) val = fn(val)
+  return val;
+})
+export const or = curry((a, b) => a || b)
 export const equals = curry((a, b) => a === b);
 export const deepEquals = (a, b) => toJson(a) === toJson(b)
 export const sum = arr => arr.reduce((sum, cv) => sum + cv, 0)
