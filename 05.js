50e56e2eaad56b8d6ffda8b8e6e05527f57941fd
Nils Riedemann <nils@bleepbloop.studio>
2021-12-06 12:20:32 +0100

    Day 05: adding ramda
    Since i didn't have time to do the challange yesterday, I wanted to get
    this done rather quickler (which i didn't) hence added ramda, so i could
    just use the toolset I know, without getting tripped up in writing own
    helper methods.
    
    At first I tried to find a way to check if to segments are intersecting
    via calculation or via complex ifs. But it always broke somewhere. So
    I went for brute force: create the lines on a grid by increasing a the
    value in a cell.
    
    Then I just need to count how many cells have a 2 or more.
    
    Of course right after implementing this, I had several ideas how to do
    it better and faster, since this takes quite a while to calculate.
    
    Welp, off to Day 06.

---
+++ 05.js
@@ -0,0 +1,99 @@
+import { map, range, repeat, zip, filter, flatten, join, transpose, tap, pipe, prop  } from "ramda";
+import { expect, readFile} from "./helpers.js";
+
+console.log("\n\n-- DAY 05--")
+const parseInput = s => s
+  .trim()
+  .split("\n")
+  .map(l => l
+    .split(",")
+    .flatMap(l => l.split(" -> "))
+    .flatMap(Number)
+  )
+  .map(l => [[l[0], l[1]], [l[2], l[3]]])
+
+const arrayPad = (a, l, f) => a.length > l ? a : a.concat(Array(l).fill(f)).slice(0, l);
+const isStraight = ([[x1, y1], [x2, y2]]) => x1 === x2 || y1 === y2
+
+const getRange = (from, to) => {
+  if (from < to) return range(from, to +1)
+  if (from > to) return range(to, from + 1).reverse()
+  return [from];
+}
+
+expect(getRange(5,3)).toEqual([5,4,3], "getRange()");
+expect(getRange(3,5)).toEqual([3,4,5], "getRange()");
+expect(getRange(0,0)).toEqual([0], "getRange()");
+
+const coords = (from, to) => {
+  let xRange = getRange(from[0], to[0])
+  let yRange = getRange(from[1], to[1])
+  if (xRange.length === 1) xRange = repeat(xRange[0], yRange.length)
+  if (yRange.length === 1) yRange = repeat(yRange[0], xRange.length)
+  return zip(xRange, yRange)
+}
+
+const drawMap = lines => lines.reduce((map, line) => {
+  const [from, to] = line;
+
+  coords(from, to).forEach(([x, y]) => {
+    if (!map[x]) {
+      map = arrayPad(map, x + 1, [])
+    }
+
+    map = map.map(row => arrayPad(row, y + 1, 0))
+    map[x][y]++
+  });
+
+  return map
+}, [])
+
+
+const render = pipe(
+  transpose,
+  map(join(" ")),
+  join("\n"),
+  tap(m => console.log(m)),
+)
+
+const countOverlaps = min => pipe(
+  flatten,
+  filter(n => n >= min),
+  prop('length'),
+)
+
+readFile("./05.sample")
+  .then(pipe(
+    parseInput,
+    filter(isStraight),
+    drawMap,
+    countOverlaps(2),
+    result => expect(result).toEqual(5, "Day 05, Part A: Sample ok")
+  ))
+
+readFile("./05.sample")
+  .then(pipe(
+    parseInput,
+    drawMap,
+    tap(render),
+    countOverlaps(2),
+    result => expect(result).toEqual(12, "Day 05, Part B: Sample ok")
+  ))
+
+readFile("./05.input")
+  .then(pipe(
+    parseInput,
+    filter(isStraight),
+    drawMap,
+    countOverlaps(2),
+    result => console.log("Day 05, Part A Result: ", result)
+  ))
+
+readFile("./05.input")
+  .then(pipe(
+    parseInput,
+    drawMap,
+    countOverlaps(2),
+    result => console.log("Day 05, Part B Result: ", result)
+  ))
+
