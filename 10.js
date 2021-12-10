290974523cb910f93c44a2e820d5ba729a9f086f
Nils Riedemann <nils@bleepbloop.studio>
2021-12-10 23:36:37 +0100

    spimplify part a
    In part b i realized that I can implement this without recursion by using a stack. Much more efficient and simpler.

--- 10.js
+++ 10.js
@@ -8,18 +8,19 @@ const pair = { "(": ")", "{": "}", "<": ">", "[": "]" }
 const points = { ")": 3, "]": 57, "}": 1197, ">": 25137 }
 const scores = { ")": 1, "]": 2, "}": 3, ">": 4 }
 
-const findErrorChars = (remaining, tree = [], pos = 0) => {
-  while (remaining.length) {
-    const [c, ...r] = remaining
-    if (isClosing(c) && c !== pair[tree[0]]) throw c
-    if (isOpening(c)) remaining = findErrorChars(r, [c, tree], pos + 1)
-    if (isClosing(c) && c === pair[tree[0]]) return r
-  }
+const findErrorChars = pipe(
+  split(""),
+  reduceWhile(
+    n => typeof n !== "string",
+    (acc, cv) => {
+      if (isClosing(cv) && cv !== head(acc)) return cv
+      if (isClosing(cv)) return slice(1, Infinity, acc)
+      return [pair[cv], ...acc]
+    },
+    []
+  )
+)
 
-  return remaining
-}
-
-const getErrorScore = pipe(tryCatch(findErrorChars, p => points[p]))
 const completeBrackets = pipe(
   split(""),
   reduce((acc, cv) => (isClosing(cv) ? acc.slice(1) : [pair[cv], ...acc]), []),
@@ -31,22 +32,27 @@ const getBracketScore = pipe(
   reduce((total, cv) => total * 5 + scores[cv], 0)
 )
 
-const getMiddle = converge(nth, [
+const getMiddleValue = converge(nth, [
   pipe(prop("length"), multiply(0.5), Math.floor),
   sort(subtract),
 ])
 
-const partA = pipe(map(getErrorScore), reject(isNil), sum)
+const partA = pipe(
+  map(findErrorChars),
+  reject(r => typeof r === "object"),
+  map(c => points[c]),
+  sum
+)
+
 const partB = pipe(
-  map(input => {
-    try {
-      findErrorChars(input)
-      return completeBrackets(input)
-    } catch {}
-  }),
-  reject(isNil),
+  reduce(
+    (acc, input) => 
+      typeof findErrorChars(input) === "object" 
+        ? acc.concat(completeBrackets(input))
+        : acc
+    , []),
   map(getBracketScore),
-  getMiddle
+  getMiddleValue
 )
 
 // -- RUNNING -----------------------------------------------------------------
@@ -73,12 +79,22 @@ aoc
           tap(aoc.expectEqual(392139, "Part A, result: ok"))
         )
       ),
-      tap(pipe(partB, tap(log("Part B, result:"))))
+      tap(
+        pipe(
+          partB,
+          tap(log("Part B, result:")),
+          tap(expectEqual(4001832844, "PartB, result: ok"))
+        )
+      )
     )
   )
 
 // -- TESTS -------------------------------------------------------------------
-
+expect(findErrorChars("{([(<{}[<>[]}>{[]{[(<()>")).toEqual("}")
+expect(findErrorChars("[[<[([]))<([[{}[[()]]]")).toEqual(")")
+expect(findErrorChars("[{[{({}]{}}([{[{{{}}([]")).toEqual("]")
+expect(findErrorChars("[<(<(<(<{}))><([]([]()")).toEqual(")")
+expect(findErrorChars("<{([([[(<>()){}]>(<<{{")).toEqual(">")
 expect(completeBrackets("[(){")).toEqual("}]")
 expect(completeBrackets("[({(<(())[]>[[{[]{<()<>>")).toEqual("}}]])})]")
 expect(completeBrackets("[(()[<>])]({[<{<<[]>>(")).toEqual(")}>]})")
@@ -90,4 +106,4 @@ expect(getBracketScore(")}>]})")).toEqual(5566)
 expect(getBracketScore("}}>}>))))")).toEqual(1480781)
 expect(getBracketScore("]]}}]}]}>")).toEqual(995444)
 expect(getBracketScore("])}>")).toEqual(294)
-expect(getMiddle([1, 2, 3, 4, 5])).toEqual(3)
+expect(getMiddleValue([1, 2, 3, 4, 5])).toEqual(3)
