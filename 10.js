797b0320d7c572761d71fb99e5cffd6347d64098
Nils Riedemann <nils@bleepbloop.studio>
2021-12-10 22:28:08 +0100

    cleanup

--- 10.js
+++ 10.js
@@ -1,7 +1,4 @@
-import { divide, multiply, nth, tryCatch } from "ramda"
 import * as aoc from "./helpers.js"
-import t from "tap"
-
 aoc.setup()
 
 const parseFile = pipe(trim, split("\n"))
@@ -9,6 +6,7 @@ const isOpening = test(/[\[\{\(\<]/)
 const isClosing = test(/[\]\}\)\>]/)
 const pair = { "(": ")", "{": "}", "<": ">", "[": "]" }
 const points = { ")": 3, "]": 57, "}": 1197, ">": 25137 }
+const scores = { ")": 1, "]": 2, "}": 3, ">": 4 }
 
 const findErrorChars = (remaining, tree = [], pos = 0) => {
   while (remaining.length) {
@@ -24,59 +22,42 @@ const findErrorChars = (remaining, tree = [], pos = 0) => {
 const getErrorScore = pipe(tryCatch(findErrorChars, p => points[p]))
 const completeBrackets = pipe(
   split(""),
-  reduce(
-    (acc, cv) => 
-      isClosing(cv) ? acc.slice(1) : [pair[cv], ...acc],
-    []
-  ),
+  reduce((acc, cv) => (isClosing(cv) ? acc.slice(1) : [pair[cv], ...acc]), []),
   join("")
 )
 
-const scores = {
-  ')': 1,
-  ']': 2,
-  '}': 3,
-  '>': 4
-}
-
 const getBracketScore = pipe(
   split(""),
-  reduce((total, cv) => (total * 5) + scores[cv],0)
+  reduce((total, cv) => total * 5 + scores[cv], 0)
 )
 
-const getMiddle = converge(nth, [pipe(prop("length"), multiply(0.5), Math.floor), sort(subtract)])
+const getMiddle = converge(nth, [
+  pipe(prop("length"), multiply(0.5), Math.floor),
+  sort(subtract),
+])
 
-const partA = pipe(
-
-        map(getErrorScore),
-        reject(isNil),
-        sum,
-)
-
+const partA = pipe(map(getErrorScore), reject(isNil), sum)
 const partB = pipe(
-        map(input => {
-          try {
-            findErrorChars(input)
-            return completeBrackets(input)
-          } catch {}
-        }),
-        reject(isNil),
-        map(getBracketScore),
-        getMiddle,
+  map(input => {
+    try {
+      findErrorChars(input)
+      return completeBrackets(input)
+    } catch {}
+  }),
+  reject(isNil),
+  map(getBracketScore),
+  getMiddle
 )
+
+// -- RUNNING -----------------------------------------------------------------
+
 aoc
   .readFile("./10.sample")
   .then(parseFile)
   .then(
     pipe(
-      tap(pipe(
-        partA,
-        tap(aoc.expectEqual(26397, "Part A, sample: ok"))
-      )),
-      tap(pipe(
-        partB,
-        aoc.expectEqual(288957, "Part B, sample: ok")
-      ))
+      tap(pipe(partA, tap(aoc.expectEqual(26397, "Part A, sample: ok")))),
+      tap(pipe(partB, aoc.expectEqual(288957, "Part B, sample: ok")))
     )
   )
 
@@ -92,14 +73,11 @@ aoc
           tap(aoc.expectEqual(392139, "Part A, result: ok"))
         )
       ),
-      tap(
-        pipe(
-          partB,
-          tap(log("Part B, result:")),
-        )
-      )
+      tap(pipe(partB, tap(log("Part B, result:"))))
     )
   )
+
+// -- TESTS -------------------------------------------------------------------
 
 expect(completeBrackets("[(){")).toEqual("}]")
 expect(completeBrackets("[({(<(())[]>[[{[]{<()<>>")).toEqual("}}]])})]")
@@ -107,10 +85,9 @@ expect(completeBrackets("[(()[<>])]({[<{<<[]>>(")).toEqual(")}>]})")
 expect(completeBrackets("(((({<>}<{<{<>}{[]{[]{}")).toEqual("}}>}>))))")
 expect(completeBrackets("{<[[]]>}<{[{[{[]{()[[[]")).toEqual("]]}}]}]}>")
 expect(completeBrackets("<{([{{}}[<[[[<>{}]]]>[]]")).toEqual("])}>")
-expect(getBracketScore( "}}]])})]"  ) ).toEqual( 288957 )
-expect(getBracketScore( ")}>]})") ).toEqual( 5566 )
-expect(getBracketScore("}}>}>))))")).toEqual( 1480781 )
+expect(getBracketScore("}}]])})]")).toEqual(288957)
+expect(getBracketScore(")}>]})")).toEqual(5566)
+expect(getBracketScore("}}>}>))))")).toEqual(1480781)
 expect(getBracketScore("]]}}]}]}>")).toEqual(995444)
-expect(getBracketScore("])}>")).toEqual(294);
-expect(getMiddle([1,2,3,4,5])).toEqual(3)
-expect(getMiddle([ 288957, 5566, 1480781, 995444, 294 ])).toEqual(288957, 'getmiddle')
+expect(getBracketScore("])}>")).toEqual(294)
+expect(getMiddle([1, 2, 3, 4, 5])).toEqual(3)
