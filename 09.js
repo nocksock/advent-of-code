7560a9bd4bff215ba2fe3dfc70c36f2383b7dc8d
Nils Riedemann <nils@bleepbloop.studio>
2021-12-11 00:26:51 +0100

    fix day09 part a

--- 09.js
+++ 09.js
@@ -1,62 +1,43 @@
 import * as aoc from "./helpers.js"
-import { writeFile } from "fs/promises"
-import { multiply, subtract } from "ramda"
 
 aoc.setup()
 
-const flag = n => 0b1_0000 | n
-const unflag = n => 0b1_0000 ^ n
-const isFlagged = n => (n & (1 << 4)) != 0
-
 const Grid = init => ({
-  map: (fn) => init.map((rows, y) => rows.map((value, x) => fn(value, [x, y]))),
-  at: (x, y) => init?.[x]?.[y],
+  map: fn => init.map((rows, y) => rows.map((value, x) => fn(value, [x, y]))),
+  at: (x, y) => init?.[y]?.[x],
   n: (x, y) => Grid(init).at(x, y - 1),
   e: (x, y) => Grid(init).at(x + 1, y),
   s: (x, y) => Grid(init).at(x, y + 1),
   w: (x, y) => Grid(init).at(x - 1, y),
   of: array => Grid(array),
   toString: () => `Grid(${init})`,
-  adjacents: (x, y) => ([
+  adjacents: (x, y) => [
     Grid(init).n(x, y),
     Grid(init).e(x, y),
     Grid(init).s(x, y),
-    Grid(init).w(x, y)
-  ])
+    Grid(init).w(x, y),
+  ],
 })
 Grid.of = a => Grid(a)
 
-const parseInput = pipe(trim, split("\n"), map(pipe(split(""), map(Number))), Grid.of)
-const calcRisk = pipe(flatten, filter(isFlagged), map(unflag), map(add(1)), sum)
+const parseInput = pipe(
+  trim,
+  split("\n"),
+  map(pipe(split(""), map(Number))),
+  Grid.of
+)
 
-const ltOrUndef = value => n => n === undefined || n <= value
-const flagLowPoints = grid =>
-  grid.map((value, [x, y]) => {
-    return any(ltOrUndef(value), grid.adjacents(x, y)) ? value : value
-  })
+const findLowPoints = grid =>
+  grid
+    .map((value, [x, y]) =>
+      filter(a => a !== undefined && a <= value, grid.adjacents(x, y)).length > 0
+        ? false
+        : [x, y]
+    )
+    .flat()
+    .filter(identity)
 
-const notEnd = v => !isFlagged(v) && ![undefined, 9].includes(v)
-
-const walk = (grid, cursor, x, y, values = []) => {
-  const adjacents = grid.adjancents(x, y).filter(notEnd)
-
-  values.concat(adjacents)
-
-  // if (notEnd(x, y - 1, n))
-  //   return walk(grid, cursor, x, y-1, values)
-
-  // if (notEnd(x + 1, y, e)) 
-  //   return walk(grid, cursor, x+1, y, values)
-
-  // if (notEnd(x - 1, y, w)) 
-  //   return walk(grid, cursor, x-1, y, values)
-
-  // if (notEnd(x, y + 1, s)) 
-  //   return walk(grid, cursor, x, y+1, values)
-
-  return values.flat(Infinity).map(unflag);
-}
-
+// const notEnd = v => !isFlagged(v) && ![undefined, 9].includes(v)
 // const findBasins = flaggedGrid => {
 //   const cursor = makeCursor(flaggedGrid);
 //   return flaggedGrid.map((row, y) => {
@@ -68,20 +49,24 @@ const walk = (grid, cursor, x, y, values = []) => {
 //   })
 // }
 
+const partA = pipe(
+  converge(
+    (lp, grid) => lp.map(pipe(apply(grid.at))),
+    [findLowPoints, identity]
+  ),
+  map(add(1)), sum
+)
+
 readFile("09.sample")
   .then(parseInput)
-  .then(
-    pipe(flagLowPoints, inspect, calcRisk, expectEqual(15, "Day 09, Part A sample: Ok")),
-    // pipe(flagLowPoints, findBasins, inspect)
-  )
+  .then(pipe(partA, expectEqual(15, "Day 09, Part A sample: Ok")))
 
 readFile("09.input")
   .then(parseInput)
   .then(
-    // pipe(
-    //   flagLowPoints,
-    //   calcRisk,
-    //   tap(log("Day 09, Part A result:")),
-    //   tap(expectEqual(506, "Day 09, Part result: ok"))
-    // )
+    pipe(
+      partA,
+      tap(log("Day 09, Part A result:")),
+      tap(expectEqual(506, "Day 09, Part result: ok"))
+    )
   )
