0da4269c5f1493f4a6561d424d62477db2f590a9
Nils Riedemann <nils@bleepbloop.studio>
2021-12-13 17:25:08 +0100

    🪟Day 01: fixing dayone
    it was broken after a lot of the following changes in the helper file

--- 01.js
+++ 01.js
@@ -1,9 +1,10 @@
-import { readFileSync } from "fs";
-import { pipe, map, sum, expect } from "./helpers.js";
-console.log(`\n-- DAY 01 --`)
+import * as aoc from "./helpers.js"
+aoc.setup()
 
-// -- Part A ---------------------------------------------------------------
-const inToArr = input => input.split("\n").map(i => parseInt(i, 10))
+// --- IMPLEMENTATIONS ----------------------------------------------------------------------
+
+const parseFile = pipe(s => s.trim(), split("\n"), reject(isNil), map(Number))
+
 const countIncrements = input =>
   input.reduce(
     (prev, cv) => ({
@@ -11,36 +12,32 @@ const countIncrements = input =>
       value: cv,
     }),
     { increments: 0 }
-  ).increments;
+  ).increments
 
-const getIncrements = pipe(inToArr, map(Number), countIncrements);
-
-// Verify via sample
-const sample = `199
-200
-208
-210
-200
-207
-240
-269
-260
-263`;
-
-expect(getIncrements(sample)).toEqual(7, "Day 01, Part 1: Sample ok")
-
-// Calculate with actual input
-const input = readFileSync("./01.input", "utf-8");
-console.log("Day 01, Part 1:", getIncrements(input));
-
-// -- Part B----------------------------------------------------------------
 const toTriplets = input =>
   input.reduce((acc, _, i, arr) => {
-    if (i + 3 > arr.length) return acc;
-    return acc.concat([arr.slice(i, i + 3)]);
-  }, []);
+    if (i + 3 > arr.length) return acc
+    return acc.concat([arr.slice(i, i + 3)])
+  }, [])
 
-const windowedIncrements = pipe(inToArr, toTriplets, map(sum), countIncrements);
+const windowedIncrements = pipe(toTriplets, map(sum), countIncrements)
 
-expect(windowedIncrements(sample)).toEqual(5, "Day 01, Part 2: Sample ok")
-console.log("Day 01, Part 2:", windowedIncrements(input));
+// --- RUNNING ----------------------------------------------------------------------
+
+readFile("./01.sample")
+  .then(parseFile)
+  .then(
+    juxt([
+      pipe(countIncrements, expectEqual(7, "Part 1: Sample ok")),
+      pipe(windowedIncrements, expectEqual(5, "Part 2: Sample ok")),
+    ])
+  )
+
+readFile("./01.input")
+  .then(parseFile)
+  .then(
+    juxt([
+      pipe(countIncrements, log("Part 1, Result:")),
+      pipe(windowedIncrements, log("Part 2, Result:")),
+    ])
+  )
