c7ece791dc18c8a302a6adb72f71e3fb5cac714a
Nils Riedemann <nils@bleepbloop.studio>
2022-12-06 10:16:58 +0100

    shorter day 6 in ts

--- years/2022/with-ts/06.ts
+++ years/2022/with-ts/06.ts
@@ -1,15 +1,12 @@
 import { readFile } from "fs/promises";
-import { juxt } from "ramda";
+import { juxt, split } from "ramda";
 
 const isMarker = (group: string[]) => new Set(group).size === group.length;
 
-const findMarkerIndex = (groupSize: number) => (input: string) =>
-  input
-    .split("")
-    .findIndex((_char, cursor, stack) =>
-      isMarker(stack.slice(cursor, cursor + groupSize))
-    ) + groupSize;
+const findMarkerIndex = (size: number) => (input: string[]) =>
+  input.findIndex((_, c, s) => isMarker(s.slice(c, c + size))) + size;
 
 readFile("../06.input", "utf-8")
+  .then(split(""))
   .then(juxt([findMarkerIndex(4), findMarkerIndex(14)]))
   .then(console.log);
