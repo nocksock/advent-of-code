51e406e41451c1859da6628e30adb76b1b50cda0
Nils Riedemann <nils@bleepbloop.studio>
2022-12-06 10:02:24 +0100

    add utils file

---
+++ years/2022/with-ts/utils.ts
@@ -0,0 +1,46 @@
+import { performance } from "perf_hooks";
+
+type Fn = <T, R>(input: T) => R;
+export const debug =
+  (label?: string) =>
+  <T>(input: T): T => {
+    console.log(`(${label || ""})::[${typeof input}]:`, input);
+    return input;
+  };
+
+export const splitLines = (input: string) => input.split("\n");
+
+export const arrayFromGroupedLines = (input: string) =>
+  input.split("\n\n").map(splitLines);
+
+export const tee =
+  <T>(fn: (input: T) => any) =>
+  (input: T) => {
+    fn(input);
+    return input;
+  };
+
+export const teeLog = tee(console.log);
+
+const obs = new PerformanceObserver((list) => {
+  list
+    .getEntries()
+    .map((entry) => console.log(`${entry.name}: ${entry.duration}`));
+
+  performance.clearMarks();
+  performance.clearMeasures();
+  obs.disconnect();
+});
+obs.observe({ entryTypes: ["function"] });
+
+export const asAsync =
+  <F extends Fn, R>(fn: F) =>
+  (input: Parameters<F>) =>
+    new Promise((resolve) => resolve(fn(input))) as Promise<R>;
+
+performance.timerify;
+
+export const parallel =
+  <F extends Fn>(...fn: F[]) =>
+  (input: Parameters<typeof fn[0]>) =>
+    Promise.all(fn.map(asAsync).map((fn) => fn(input)));
